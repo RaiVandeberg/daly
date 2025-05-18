f589feed300c1e61dab5b04ecd0647b4a63acd4a
Raí Vandenberg <rvdesigner20@gmail.com>
2025-05-18 10:55:58 -0300

    navbar created

---
+++ src/components/header/index.tsx
@@ -0,0 +1,38 @@
+import LogoImg from "@/public/logo.svg"
+import { tree } from "next/dist/build/templates/app-page"
+import Image from 'next/image'
+import Link from 'next/link'
+import { LiaGamepadSolid } from "react-icons/lia"
+
+export function Header(){
+    return (
+        <header className="w-full h-28 bg-slate-100 text-black px-2">
+           <div className="max-w-screen-xl mx-auto flex justify-center items-center h-28 sm:justify-between">
+            <nav className="flex  justify-center items-center gap-4">
+                <Link href={"/"}>
+                <Image src={LogoImg}
+                alt='Logo do Site'
+                priority={true}
+                className="w-full" />
+                
+                </Link>
+
+                <Link href={"/"}>
+                Games
+                </Link>
+
+                  <Link href={"/"}>
+                Perfil
+                </Link>
+            </nav>
+
+            <div className="hidden sm:flex justify-center items-center">
+                <Link href={"/profile"}>
+                <LiaGamepadSolid size={34} color="#475569"/>
+                </Link>
+            </div>
+
+           </div>
+        </header>
+    )
+}
